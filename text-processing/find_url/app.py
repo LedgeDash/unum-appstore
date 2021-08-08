f30d27e538fa481c27b3bf14fbee7039d6168917
David H. Liu <hl7@cs.princeton.edu>
2021-08-08 17:11:43 -0500

    text processing app

---
+++ text-processing/find_url/app.py
@@ -0,0 +1,4 @@
+import json
+
+def lambda_handler(event, context):
+    return ["https://thenationalpulse.com/news/abcs-trump-in-trouble-poll-surveyed-just-533-not-likely-voters-asked-over-20-more-biden-supporters-than-conservatives"]
